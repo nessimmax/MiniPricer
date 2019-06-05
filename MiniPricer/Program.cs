030bc9c144d1cee9dfa736f2d6f9041c54451bf2
nessimmax <nessimmax@gmail.com>
2019-06-05 13:09:53 +0200

    Corrections parse et affichage des données avec locale

--- MiniPricer/Program.cs
+++ MiniPricer/Program.cs
@@ -11,34 +11,45 @@ namespace MiniPricer
         static void Main(string[] args)
         {
 
-            double P = 0;
-            double V = 0;
+            double prix = 0;
+            double volatilite = 0;
 
-            Console.Write("Le Prix acteul de produit : ");
-            double.TryParse(Console.ReadLine(), out P);
+            System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("fr-FR");
+
+            Console.Write("Le Prix actuel du produit : ");
+            if (!double.TryParse(Console.ReadLine(), out prix))
+                return;
+
             Console.Write("Volatilité journalière moyenne de l’instrument en %: ");
-            double.TryParse(Console.ReadLine(), out V);
+            if (!double.TryParse(Console.ReadLine(), out volatilite))
+                return;
 
 
-            Console.Write("Entez la date (s.forme 10/24/2019): ");
-            DateTime DatePrevisonnel = DateTime.Parse(Console.ReadLine());
+            Console.Write("Entez la date (s.forme 24/10/2019): ");
+            DateTime datePrevisonnelle;
+            if (!DateTime.TryParse(Console.ReadLine(), out datePrevisonnelle))
+                return;
 
             Console.Write("Entez date jour ferié ou bien laissez vide (s.forme 10/24/2019): ");
             Product p = new Product();
-            p.Prix = P;
-            p.Volatilite = V;
+            p.Prix = prix;
+            p.Volatilite = volatilite;
 
             var console = Console.ReadLine();
 
-            while (console !="")
+            while (console != String.Empty)
             {
-                p.Feries.Add(DateTime.Parse(console));
+                DateTime jourFerie;
+                if (!DateTime.TryParse(console, out jourFerie))
+                    return;
+
+                p.Feries.Add(jourFerie);
                 Console.Write("Entez une autre date jour ferié ou bien laissez vide (s.forme 10/24/2019): ");
                 console = Console.ReadLine();
             }
 
 
-            Console.Write("Le prix {0} est le {1}", p.GetPrixPrev(DatePrevisonnel), DatePrevisonnel.ToString("dd/MM/yyyy"));
+            Console.Write("Le prix {0:C} est le {1:dd/MM/yyyy}", p.GetPrixPrev(datePrevisonnelle), datePrevisonnelle);
             Console.ReadKey();
         }
     }
